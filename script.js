fc6d16a5dd49a71c88b2a1b6d01bb86fe56634df
Eduardo Henrique <eduardohenriq0612002@gmail.com>
2023-02-07 12:27:15 -0300

    :tada: ok

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ function toggle() {
   const html = document.documentElement;
 
   html.classList.toggle('light')
+
+  if(html.classList.contains('light')){
+    document.getElementById('img-curriculo').src = "https://img.icons8.com/ios-glyphs/1024/000000/resume.png"
+  } else {
+    document.getElementById('img-curriculo').src = "https://img.icons8.com/ios-glyphs/1024/ffffff/resume.png"
+  }
 }
